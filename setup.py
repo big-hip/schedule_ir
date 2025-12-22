843b8b3f94214dafbcd544e3aa799714e914ea7c
tcx <128908427+big-hip@users.noreply.github.com>
2025-12-22 14:32:08 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='schedule_irV',
+    version='0.1',
+    packages=find_packages(),  # 自动查找所有包
+    package_data={},
+    install_requires=[],
+)
